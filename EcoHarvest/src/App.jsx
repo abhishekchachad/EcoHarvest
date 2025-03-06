e259176cfde80749f1dfbd9c2dab6ddb18292327
Gaurav <gauravchumber20@gmail.com>
2025-03-06 10:54:47 -0500

    Final sprint 2

--- EcoHarvest/src/App.jsx
+++ EcoHarvest/src/App.jsx
@@ -1,25 +1,22 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import HomePage from "./pages/Home";
 import Products from "./pages/Products";
-import AddProduct from "./pages/AddProduct";
 import ContactUs from "./pages/ContactUs";
 import AboutUs from "./pages/AboutUs";
-import AdminPage from "./pages/AdminPage"; // Import Admin Page
+import AdminPage from "./pages/AdminPage";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/add-product" element={<AddProduct />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/admin" element={<AdminPage />} /> {/* Admin Route */}
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
       <Footer />
     </Router>
