483917d454ace3308e0e4e2e9ff48b0c4f49eb44
Abhishek Ajay Chachad <abhishek.chachad@gmail.com>
2025-03-26 12:56:51 -0400

    update server.js
    removed conflict

--- EcoHarvest_Backend/server.js
+++ EcoHarvest_Backend/server.js
@@ -31,13 +31,8 @@ const jwt = require("jsonwebtoken");
 const { poolPromise } = require("./config/dbConfig"); // Import the poolPromise from your dbconfig
 const cartRoutes = require("./routes/cartRoutes");
 const orderRoutes = require("./routes/orderRoutes");
-<<<<<<< Updated upstream
 const productRoutes = require("./routes/productRoutes");
-=======
-const productRoutes = require("./routes/productRoutes"); // Ensure correct path
 const messageRoutes = require("./routes/messageRoutes");
->>>>>>> Stashed changes
-
 const app = express();
 
 app.use(express.json());
@@ -48,7 +43,6 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 
-<<<<<<< Updated upstream
 // Signup Route
 app.post("/api/signup", async (req, res) => {
   const { email, password, username, role = 'customer' } = req.body; // Default role is 'customer'
@@ -126,10 +120,8 @@ app.post("/api/login", async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
-=======
-app.use("/api/messages", messageRoutes);
->>>>>>> Stashed changes
 
+app.use("/api/messages", messageRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
