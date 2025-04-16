9d022d5520fb8d9cfcde5770f6292ce18c86a678
Abhishek Ajay Chachad <abhishek.chachad@gmail.com>
2025-04-16 00:27:41 -0400

    Update cart.js

--- EcoHarvest_Backend/api/cart.js
+++ EcoHarvest_Backend/api/cart.js
@@ -1,9 +1,21 @@
 // api/cart.js
-import { setCorsHeaders } from '../config/setCorsHeaders';
+// import { setCorsHeaders } from '../config/setCorsHeaders';
 
 export default async function handler(req, res) {
-  
-  setCorsHeaders (res); // Set CORS headers
+
+  const origin = req.headers.get("origin") || "*";
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", origin || "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", req.headers["access-control-request-headers"] || "*");
+
+  //setCorsHeaders (res); // Set CORS headers
+  if (req.method === "OPTIONS") {
+    res.status(200).end(); // Stop preflight here
+    return;
+  }
+
+  // setCorsHeaders (res); // Set CORS headers
     if (req.method === 'GET') {
       // Logic for adding items to the cart
       res.status(200).json({ message: 'Product added to cart' });
